76811b1f0c7b0dc1e84c1ec63b86412f530a96ec
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2021-11-17 15:32:11 +0100

    added products to the page. every product is an object that has name, price, img and notAvalible properties.

---
+++ src/products.js
@@ -0,0 +1,40 @@
+import xboxSeriesXImg from './images/xboxX.png';
+import xboxSeriesSImg from './images/seriess.png';
+import playstation5Img from './images/ps5';
+import switchImg from './images/switch';
+
+const xboxSeriesX = {
+    name: 'Xbox Series X',
+    price: "399",
+    img: xboxSeriesXImg,
+    notAvalible: false,
+}
+
+const xboxSeriesS = {
+    name: 'Xbox Series S',
+    price: "299",
+    img: xboxSeriesSImg,
+    notAvalible: false,
+}
+
+const playstation5 = {
+    name: 'Sony Playstation 5',
+    price: "499",
+    img: playstation5Img,
+    notAvalible: false,
+}
+
+const nintendoSwitchRegular = {
+    name: 'Nintendo Switch',
+    price: "299",
+    img: switchImg,
+    notAvalible: false,
+}
+
+const nintendoSwitchOled = {
+    name: 'Nintendo Switch OLED',
+    price: "349",
+    notAvalible: false,
+}
+
+export { xboxSeriesS, xboxSeriesX, playstation5, nintendoSwitchOled, nintendoSwitchRegular}
